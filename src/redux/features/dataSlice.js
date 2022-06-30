8bda9b12f75509dc36a43c08020ab07a7894b317
orioch <orioch.work@gmail.com>
2022-06-30 15:06:59 +0300

    fetch the data to the state

--- src/redux/features/dataSlice.js
+++ src/redux/features/dataSlice.js
@@ -1,10 +1,36 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 
-const initialState = {};
+const url = "https://randomuser.me/api/?results=10";
+const initialState = {
+  users: [],
+  info: {},
+  isLoading: false,
+};
+
+export const getUsersData = createAsyncThunk("users/getUsersData", () => {
+  return fetch(url)
+    .then((res) => res.json())
+    .catch((err) => console.log(err));
+});
 
 const dataSlice = createSlice({
   name: "data",
   initialState,
+  reducers: {},
+  extraReducers: {
+    [getUsersData.pending]: (state) => {
+      state.isLoading = true;
+    },
+    [getUsersData.fulfilled]: (state, action) => {
+      console.log(action);
+      state.isLoading = false;
+      state.users = action.payload.results;
+      state.info = action.payload.info;
+    },
+    [getUsersData.pending]: (state) => {
+      state.isLoading = false;
+    },
+  },
 });
 
 export default dataSlice.reducer;
